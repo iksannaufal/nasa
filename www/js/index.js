25a031447bc16c35ae446008d3eadfd7bf11e489
iksannaufal <48430047+iksannaufal@users.noreply.github.com>
2019-04-15 09:12:24 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=hafvpzy5R9BJDPR72qIMcPdpf5bhVFdFU2xPgiQ8";
         
         $("#loadImage").click(function(){
             $.ajax({
